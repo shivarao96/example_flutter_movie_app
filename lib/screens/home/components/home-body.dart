7de463966704659ff2fb68215edfa71d3b2e1650
A Shiva Rao <shivarao96@gmail.com>
2021-03-30 17:16:31 +0530

    movie poster card added

--- lib/screens/home/components/home-body.dart
+++ lib/screens/home/components/home-body.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/constants.dart';
+import 'package:flutter_movie_app/models/movies.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'category.dart';
 import 'genre.dart';
+import 'movie_card.dart';
 
 class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[CategoryList(), GenereComponent()],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[CategoryList(), GenereComponent(), MovieCarousel()],
+      ),
+    );
+  }
+}
+
+class MovieCarousel extends StatefulWidget {
+  @override
+  _MovieCarouselState createState() => _MovieCarouselState();
+}
+
+class _MovieCarouselState extends State<MovieCarousel> {
+  PageController _pageController;
+  int intialPage = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(appDefaultPadding),
+      child: AspectRatio(
+          aspectRatio: 0.95,
+          child: PageView.builder(
+              itemCount: movies.length,
+              itemBuilder: (context, index) => MovieCard(
+                    movie: movies[index],
+                  ))),
     );
   }
 }
